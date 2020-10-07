f1c5aa5c63245193ce7ec26b8a97d71252679952
adam <athornton@gmail.com>
2020-10-07 14:12:15 -0700

    instrument labextension init

--- dask_labextension/__init__.py
+++ dask_labextension/__init__.py
@@ -1,5 +1,7 @@
 """A Jupyter notebook server extension for managing Dask clusters."""
 
+import logging
+
 from notebook.utils import url_path_join
 
 from . import config
@@ -27,7 +29,8 @@ def load_jupyter_server_extension(nb_server_app):
     cluster_id_regex = r"(?P<cluster_id>\w+-\w+-\w+-\w+-\w+)"
     web_app = nb_server_app.web_app
     base_url = web_app.settings["base_url"]
-    get_cluster_path = url_path_join(base_url, "dask/clusters/" + cluster_id_regex)
+    get_cluster_path = url_path_join(
+        base_url, "dask/clusters/" + cluster_id_regex)
     list_clusters_path = url_path_join(base_url, "dask/clusters/" + "?")
     get_dashboard_path = url_path_join(
         base_url, f"dask/dashboard/{cluster_id_regex}(?P<proxied_path>.+)"
@@ -37,4 +40,7 @@ def load_jupyter_server_extension(nb_server_app):
         (list_clusters_path, DaskClusterHandler),
         (get_dashboard_path, DaskDashboardHandler),
     ]
+    logging.warning("get_cluster_path: {}".format(get_cluster_path))
+    logging.warning("list_clusters_path: {}".format(list_clusters_path))
+    logging.warning("get_dashboard_path: {}".format(get_dashboard_path))
     web_app.add_handlers(".*$", handlers)
